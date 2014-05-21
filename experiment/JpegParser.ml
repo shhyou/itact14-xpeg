be7229c31e273f57aabb7d28b9d9a3ef3a12ee8f
suhorng <suhorng@gmail.com>
2014-05-21 15:03:20 +0800

    add JPEG parsing

--- experiment/JpegParser.ml
+++ experiment/JpegParser.ml
@@ -2,11 +2,7 @@ open Prelude;;
 
 (* jpeg parse *)
 
-(* TODO
- * - huffman tables
- * - quantisation table
- * - SOF (start of fragment): height, width, components
- * - SOS (start of segment) : dctable, actable, ncomponents *)
+module L = List;;
 
 let jpeg_raw =
   let fin = open_in_bin "hd.jpg" in
@@ -19,26 +15,26 @@ let jpeg_raw =
 exception Jpeg_format_error of string;;
 
 let jpeg_segmenting raw_data =
-  let [ch_ff; ch_00; ch_EOI] = List.map char_of_int [0xff; 0x00; 0xd9]
+  let [ch_ff; ch_00; ch_EOI] = L.map char_of_int [0xff; 0x00; 0xd9]
   in let rec find_candidates acc start_idx =
     try let idx = String.index_from raw_data start_idx ch_ff
         in if raw_data.[idx+1] == ch_EOI
-             then List.rev acc
+             then L.rev acc
              else find_candidates (idx::acc) (idx+1)
     with Not_found -> raise (Jpeg_format_error "EOI (0xff 0xd9) not found")
   in let parse_result =
        find_candidates [] 0
-    |> List.filter (fun i -> raw_data.[i+1] <> ch_00 && raw_data.[i+1] <> ch_ff)
-    |> List.map (fun i -> (int_of_char raw_data.[i+1], i+2))
+    |> L.filter (fun i -> raw_data.[i+1] <> ch_00 && raw_data.[i+1] <> ch_ff)
+    |> L.map (fun i -> (int_of_char raw_data.[i+1], i+2))
   in match parse_result with
       (0xd8, 2)::rest -> rest
     | _ -> raise (Jpeg_format_error "SOI (0xff 0xd8) not found");;
 
 let zigzag_order () =
   let skew_diag y0 x0 =
-    (if (y0+x0) mod 2 == 0 then (fun x -> x) else List.rev)
-    (List.map (fun i -> (y0-i, x0+i)) (range 0 (min y0 (7-x0) + 1))) in
-  List.map2 skew_diag (range 0 8@[7;7;7;7;7;7;7]) ([0;0;0;0;0;0;0]@range 0 8);;
+    (if (y0+x0) mod 2 == 0 then (fun x -> x) else L.rev)
+    (L.map (fun i -> (y0-i, x0+i)) (range 0 (min y0 (7-x0) + 1))) in
+  L.map2 skew_diag (range 0 8@[7;7;7;7;7;7;7]) ([0;0;0;0;0;0;0]@range 0 8);;
 
 (* parse one 16-bit bigendian number *)
 let u16_of_char raw_data idx =
@@ -61,7 +57,7 @@ let jpeg_parse_dqt raw_data dqt_idx =
     { dqt_id = table_id; dqt_idx = idx+1 } in
   if 65 mod size <> 0
     then raise (Jpeg_format_error "DQT table size is not a multiple of 65")
-    else List.map (fun i -> parse_table (dqt_idx+2 + i*65)) (range 0 (65/size));;
+    else L.map (fun i -> parse_table (dqt_idx+2 + i*65)) (range 0 (65/size));;
 
 (* print_binary *)
 let rec print_binary len n = match len with
@@ -79,13 +75,13 @@ type jpeg_dht = {
 let jpeg_parse_dht raw_data dht_idx =
   let size = u16_of_char raw_data dht_idx - 2 - 1 - 16 in
   let (table_type, table_id) = u4_of_char raw_data (dht_idx+2) in
-  let tree_sizes = flip List.map (range 0 16) (fun i ->
+  let tree_sizes = flip L.map (range 0 16) (fun i ->
                    int_of_char raw_data.[dht_idx+3 + i])
-                |> List.filter (fun len -> len <> 0) in
+                |> L.filter (fun len -> len <> 0) in
   let acts =
-    List.concat
-    (flip List.mapi tree_sizes (fun depth len ->
-    (depth+2, fun n -> (n+1) lsl 1)::List.map (fun _ -> (depth+2, fun n -> n+1))
+    L.concat
+    (flip L.mapi tree_sizes (fun depth len ->
+    (depth+2, fun n -> (n+1) lsl 1)::L.map (fun _ -> (depth+2, fun n -> n+1))
                                               (range 0 (len-1)))) in
   let tbl = Array.make 32768 (0, 0) in
   let set_code prev_code (depth, act, data) =
@@ -95,9 +91,9 @@ let jpeg_parse_dht raw_data dht_idx =
     done;
     code
   in range 0 size
-  |> List.map2 (fun (depth, act) i ->
+  |> L.map2 (fun (depth, act) i ->
      (depth, act, int_of_char raw_data.[dht_idx+19 + i])) acts
-  |> List.fold_left set_code (-1);
+  |> L.fold_left set_code (-1);
   { dht_type = table_type; dht_id = table_id; dht_tbl = tbl };;
 
 type jpeg_sof_comp = {
@@ -117,20 +113,20 @@ type jpeg_sof = {
 
 let jpeg_parse_sof raw_data sof_idx =
   let nf = int_of_char raw_data.[sof_idx+7] in
-  let [height; width] = List.map (u16_of_char raw_data) [sof_idx+3; sof_idx+5] in
+  let [height; width] = L.map (u16_of_char raw_data) [sof_idx+3; sof_idx+5] in
   let comps =
-     flip List.map (range 0 nf) (fun idx ->
+     flip L.map (range 0 nf) (fun idx ->
      let pos = sof_idx+8 + idx*3 in
      let (hi, vi) = u4_of_char raw_data (pos+1) in
      { sof_comp_id = int_of_char raw_data.[pos]
      ; sof_hi = hi; sof_vi = vi
      ; sof_comp_height = 0; sof_comp_width = 0
      ; sof_tq = int_of_char raw_data.[pos+2] land 0xf} ) in
-  let hmax = maximum (List.map (fun c -> c.sof_hi) comps) in
-  let vmax = maximum (List.map (fun c -> c.sof_vi) comps) in
+  let hmax = maximum (L.map (fun c -> c.sof_hi) comps) in
+  let vmax = maximum (L.map (fun c -> c.sof_vi) comps) in
   { sof_height = height
   ; sof_width = width
-  ; sof_comps = flip List.map comps (fun comp ->
+  ; sof_comps = flip L.map comps (fun comp ->
                 { comp with sof_comp_height = height * comp.sof_hi / hmax
                           ; sof_comp_width = width * comp.sof_vi / vmax }) };;
 
@@ -149,9 +145,28 @@ let jpeg_parse_sos raw_data sof_idx =
   let size = u16_of_char raw_data sof_idx in
   let ns = int_of_char raw_data.[sof_idx+2] in
   let comps =
-    flip List.map (range 0 ns) (fun idx ->
+    flip L.map (range 0 ns) (fun idx ->
     let (tdj, taj) = u4_of_char raw_data (sof_idx+3 + idx*2 + 1) in
     { sos_comp_sel = int_of_char raw_data.[sof_idx+3 + idx*2]
     ; sos_dc_sel = tdj
     ; sos_ac_sel = taj }) in
   { sos_comps = comps; sos_data = sof_idx + size };;
+
+type jpeg = {
+  dqts: jpeg_dqt list;
+  dhts: jpeg_dht list;
+  sof: jpeg_sof;
+  soss: jpeg_sos list;
+};;
+
+let parse_jpeg raw_data =
+  let segs = jpeg_segmenting raw_data in
+  let seg_filter marker fn =
+       segs
+    |> L.filter (fun (marker_, _) -> marker == marker_)
+    |> L.map (fun (_, pos) -> fn raw_data pos) in
+  let dqts = L.concat (seg_filter 0xdb jpeg_parse_dqt) in
+  let dhts = seg_filter 0xc4 jpeg_parse_dht in
+  let [sof] = seg_filter 0xc0 jpeg_parse_sof in
+  let soss = seg_filter 0xda jpeg_parse_sos in
+  { dqts = dqts; dhts = dhts; sof = sof; soss = soss };;
