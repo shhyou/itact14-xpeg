be0cb9ac8fb3870ba1b391bd2d887b47176378ed
suhorng <suhorng@gmail.com>
2014-05-29 15:28:19 +0800

    add building scripts

--- experiment/build.bat
+++ experiment/build.bat
@@ -8,7 +8,12 @@ ocamlopt -strict-sequence -unsafe -ffast-math -c Bitmap.ml
 @if errorlevel 1 goto bye
 ocamlopt -strict-sequence -unsafe -ffast-math -c JpegParser.ml
 @if errorlevel 1 goto bye
-ocamlopt -strict-sequence -unsafe -ffast-math -o a.exe Prelude.cmx Bitmap.cmx JpegParser.cmx
+ocamlopt -strict-sequence -unsafe -ffast-math -o JpegParser.exe Prelude.cmx Bitmap.cmx JpegParser.cmx
 @if errorlevel 1 goto bye
-D:\code\clock a teatime.jpg
+ocamlopt -strict-sequence -unsafe -ffast-math -c BmpParser.ml
+@if errorlevel 1 goto bye
+ocamlopt -strict-sequence -unsafe -ffast-math -o BmpParser.exe Prelude.cmx Bitmap.cmx BmpParser.cmx
+@if errorlevel 1 goto bye
+D:\code\clock jpegparser teatime.jpg
+D:\code\clock bmpparser  out.bmp
 :bye
